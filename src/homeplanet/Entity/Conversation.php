f3f8c90f0265b9d2e37b3be8cc2cdd96f6258992
jeremyginer <remy.giner@gmail.com>
2017-07-31 17:16:23 +0200

    Update conversation proto

--- src/homeplanet/Entity/Conversation.php
+++ src/homeplanet/Entity/Conversation.php
@@ -45,6 +45,7 @@ class Conversation {
 	 * Member :
 	 * - initiative : int character id
 	 * - points : int[][] indexed by character id then by persuade(0), coerce(1), ...
+	 * - log : 
 	 * 
 	 * @ORM\Column(type="array", name="state")
 	 */
@@ -81,6 +82,14 @@ class Conversation {
 
 	public function setState( array $aState ) {
 		return $this->_aState = $aState;
+	}
+	
+	public function processExpression( Expression $oExp0, Expression $oExp1 ) {
+		
+		$this->_aState = $oExp0->getEffect()->apply( $this->_aState );
+		$this->_aState = $oExp1->getEffect()->apply( $this->_aState );
+		
+		$this->_aState['log'][] = array( $oExp0->getId(), $oExp1->getId() );
 	}
 	
 //_____________________________________________________________________________
